4b47f6d778dc8fc4af047d8546a43e052475ff07
Pang <7981595+thepang@users.noreply.github.com>
2020-03-12 11:21:30 -0500

    variables file for other methods

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Folders where the data is
+ROOT_PATH = "/Users/pang/repos/scotus/data"
+
+# Assumed to be nested under ROOT_PATH
+HTML_FOLDER = "001_html"
+TR_META_FOLDER = "002_transcript_metadata"
+PDF_FOLDER = "003_pdfs"
+PDF_RAW_TXT_FOLDER = "004_pdf_raw"
+CLEAN_TXT_FOLDER = "005_cleaned_text"
+SPEECH_FOLDER = "006_speech"
