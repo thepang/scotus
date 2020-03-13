b21b5580599458b096ed5a7a45320d713c9d2681
Pang <7981595+thepang@users.noreply.github.com>
2020-03-13 10:50:49 -0500

    refinement of docket file

--- get_case_data.py
+++ get_case_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 import transcript_file_helper as gt
 import variables as v
 
+pd.set_option("display.max_columns", 500)
+
 # scdb data manually downloaded from http://supremecourtdatabase.org/index.php
 scdb_docket_location = "data/SCDB_2019_01_caseCentered_Docket.csv"
 
@@ -23,12 +25,61 @@ def get_case_meta_from_scdb():
 
     # Inner join because there are cases that were argued but have not been decided on.
     # The join gets us cases that went through oral argument and have received a decision.
-    case_meta = dockets_for_analysis.merge(
-        docket_meta, how="inner", on="docket"
-    ).sort_values(by="docket")
+    case_meta = (
+        dockets_for_analysis.merge(docket_meta, how="inner", on="docket")
+        .sort_values(by="docket")
+        .drop(
+            columns=[
+                # SCDB specific data and is not useful for analysis
+                "caseId",
+                "docketId",
+                "caseIssuesId",
+                "voteId",
+                # Other cite data not useful for analysis
+                "usCite",
+                "sctCite",
+                "ledCite",
+                "lexisCite",
+                # Data related to outcome that can't be used for analysis
+                "decisionType",
+                "declarationUncon",
+                "caseDisposition",
+                "caseDispositionUnusual",
+                "precedentAlteration",
+                "voteUnclear",
+                "decisionDirection",
+                "decisionDirectionDissent",
+                "authorityDecision1",
+                "authorityDecision2",
+                "majOpinWriter",
+                "majOpinAssigner",
+                "splitVote",
+                # Other data not useful for analysis
+                "chief",
+                "caseName",
+                # Too sparse. Data will require too many dummy variables.
+                "petitionerState",
+                "adminActionState",
+                "threeJudgeFdc",
+                "caseOriginState",
+                "lawSupp",  # seems redundant with lawType
+                "lawMinor",
+            ]
+        )
+    )
 
     return case_meta
 
 
+# Data of the truthe: 'majVotes', 'minVotes', 'partyWinning'
+# Update the following to dummy out the top 1-3 or to values
+# petitioner, and respondent, adminAction, certReason, issue, issueArea
+# 'lcDisagreement', 'lcDisposition', 'lcDisposition', lawType
+# Change petitionerState to just track if the case is from California, and respondentState to track if from TX
+# Change Jurisdiction to b e a bool, only two values anyways
+# caseOrigin/caseSource might be redundant with other columns. start with it only using the top few dummied out
+
 cases = get_case_meta_from_scdb()
-print(len(cases["partyWinning"].unique()))
+# print(cases)
+variable = "lawMinor"
+print(cases)
